5e066f838ac2ad44fab40e85801c5b1d6d90aa25
Dimitris Karakostas <dimit.karakostas@gmail.com>
2025-04-18 12:37:39 +0900

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'datasets.gr',
+  description: 'Συλλογές δεδομένων για την Ελλάδα',
   generator: 'v0.dev',
 }
 
