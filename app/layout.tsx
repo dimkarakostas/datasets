af26ab9d68142322a5da6a43002869261bbdbc25
Dimitris Karakostas <dimit.karakostas@gmail.com>
2025-05-21 18:47:12 +0100

    Add cloudflare analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "d0b6ff47a62e4bf2ae46108004b8641d"}'></script>
     </html>
   )
 }
